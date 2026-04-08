d754ab6a996b36bac0328f92941dc60d9152fb41
parul <cseprs@gmail.com>
2026-04-08 15:03:39 +0530

    final

--- models/gold/gold_quiz_performance.sql
+++ models/gold/gold_quiz_performance.sql
@@ -1,17 +1,17 @@
 {{ config(materialized='table') }}
 
 SELECT
-    q.quiz_id,
-    q.quiz_name,
-    q.course_id,
-    q.max_grade,
-    COUNT(DISTINCT a.user_id)                           AS total_attempts,
+    q.id                                                AS quiz_id,
+    q.name                                              AS quiz_name,
+    q.course                                            AS course_id,
+    q.grade                                             AS max_grade,
+    COUNT(DISTINCT a.userid)                            AS total_attempts,
     ROUND(AVG(CAST(a.sumgrades AS FLOAT)), 2)           AS avg_score,
     MAX(a.sumgrades)                                    AS highest_score,
     ROUND(
         100.0 * SUM(
             CASE 
-                WHEN a.sumgrades >= q.max_grade * 0.5
+                WHEN a.sumgrades >= q.grade * 0.5
                 THEN 1 ELSE 0
             END
         ) / NULLIF(CAST(COUNT(*) AS FLOAT), 0),
@@ -19,6 +19,6 @@ SELECT
     )                                                   AS pass_rate_pct
 FROM silver.[silver.quiz] q
 LEFT JOIN silver.[silver.quiz_attempts] a
-    ON q.quiz_id = a.quiz
+    ON q.id = a.quiz
 GROUP BY
-    q.quiz_id, q.quiz_name, q.course_id, q.max_grade
+    q.id, q.name, q.course, q.grade
