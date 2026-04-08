a2406a4f22f80eb240cbf46e5e5abc4074a4efa4
parul <cseprs@gmail.com>
2026-04-08 14:53:09 +0530

    updates

--- models/gold/gold_quiz_performance.sql
+++ models/gold/gold_quiz_performance.sql
@@ -1,7 +1,5 @@
 {{ config(materialized='table') }}
 
-{% set silver_schema = 'silver' %}
-
 SELECT
     q.quiz_id,
     q.quiz_name,
@@ -14,14 +12,13 @@ SELECT
         100.0 * SUM(
             CASE 
                 WHEN a.sumgrades >= q.max_grade * 0.5
-                THEN 1 
-                ELSE 0 
+                THEN 1 ELSE 0
             END
         ) / NULLIF(CAST(COUNT(*) AS FLOAT), 0),
         1
     )                                                   AS pass_rate_pct
-FROM {{ silver_schema }}.quiz q
-LEFT JOIN {{ silver_schema }}.quiz_attempts a
+FROM silver.[silver.quiz] q
+LEFT JOIN silver.[silver.quiz_attempts] a
     ON q.quiz_id = a.quiz
 GROUP BY
     q.quiz_id, q.quiz_name, q.course_id, q.max_grade
